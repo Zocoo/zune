fb7e86ffb9e43fc6aa5ea6e692a75d6e0d569058
zocoo <815769472@qq.com>
2016-01-21 16:51:31 +0800

    ss

--- src/util/email.php
+++ src/util/email.php
@@ -5,9 +5,5 @@ $mail = new mail ();
 $mail->setSmtpemailto ( "815769472@qq.com" );
 $mail->setMailsubject ( "phpemail" );
 $mail->setMailbody ( "today is a good day" );
-$mail->setSmtpusermail ( "p815769472@163.com" );
-$mail->setSmtppass ( "psoxsk" );
-$mail->setSmtpserverport ( 25 );
-$mail->setSmtpserver ( "smtp.163.com" );
 $sendmail->send ( $mail );
 ?>
--- src/util/sendmail.php
+++ src/util/sendmail.php
@@ -3,28 +3,15 @@ require ("smtp.php");
 require ("../pojo/mail.php");
 class sendmail {
 	function send($mail) {
-		if (! empty ( $mail->getSmtpemailto () )) {
-			
-			$smtpemailto = $mail->getSmtpemailto ();
-		}
-		if (! empty ( $mail->getMailsubject () )) {
-			$mailsubject = $mail->getMailsubject ();
-		}
-		if (! empty ( $mail->getMailbody () )) {
-			$mailbody = $mail->getMailbody ();
-		}
-		if (! empty ( $mail->getSmtpusermail () )) {
-			$smtpusermail = $mail->getSmtpusermail (); // SMTP服务器的用户邮箱
-			$smtpuser = $mail->getSmtpusermail (); // SMTP服务器的用户帐号
-		}
-		if (! empty ( $mail->getSmtppass () )) {
-			$smtppass = $mail->getSmtppass (); // SMTP服务器的用户密码
-		}
-		if (! empty ( $mail->getMailtype () )) {
-			$mailtype = $mail->getMailtype (); // 邮件格式（HTML/TXT）,TXT为文本邮件
-		}
-		$smtpserver = "smtp.163.com"; // SMTP服务器
-		$smtpserverport = 25; // SMTP服务器端口
+		$smtpemailto = $mail->getSmtpemailto ();
+		$mailsubject = $mail->getMailsubject ();
+		$mailbody = $mail->getMailbody ();
+		$smtpusermail = $mail->getSmtpusermail (); // SMTP服务器的用户邮箱
+		$smtpuser = $mail->getSmtpusermail (); // SMTP服务器的用户帐号
+		$smtppass = $mail->getSmtppass (); // SMTP服务器的用户密码
+		$mailtype = $mail->getMailtype (); // 邮件格式（HTML/TXT）,TXT为文本邮件
+		$smtpserver = $mail->getSmtpserver ();
+		$smtpserverport = $mail->getSmtpserverport ();
 		$smtp = new smtp ( $smtpserver, $smtpserverport, true, $smtpuser, $smtppass ); // 这里面的一个true是表示使用身份验证,否则不使用身份验证.
 		$smtp->debug = TRUE; // 是否显示发送的调试信息
 		                     // echo "===" . $mail->getSmtpemailto ();
